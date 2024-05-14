43e822638e0fd6e4fc65bab8732d85029a64d127
BriqueAuBob <briqueaubob@gmail.com>
2024-05-14 20:15:47 +0200

    :heavy_plus_sign: Setup Pinia

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router/auto";
+import { createPinia } from "pinia";
 
 const router = createRouter({
   history: createWebHistory(),
 });
 
-createApp(App).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
