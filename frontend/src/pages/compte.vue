c118a33a37e65119ef6d0ab0410b6e1f2cb4cbc7
BriqueAuBob <briqueaubob@gmail.com>
2024-05-15 23:26:26 +0200

    :lipstick: Improve margins

--- frontend/src/pages/compte.vue
+++ frontend/src/pages/compte.vue
@@ -9,12 +9,12 @@ const userStore = useUserStore();
 
 <template>
   <Layout>
-    <div class="container py-8 pt-24">
+    <div class="container py-8 pt-32">
       <h1 class="text-xl font-semibold">Compte</h1>
       <h2 class="mb-4 text-sm font-medium">Vous êtes connecté en tant que {{ userStore.user?.username }} ({{ userStore.user?.email }})</h2>
       <PreferencesList />
       <div class="flex justify-center">
-        <Button class="mx-auto mt-4" color="red" @click="userStore.logout">Déconnexion</Button>
+        <Button class="mx-auto mt-8" color="red" @click="userStore.logout">Déconnexion</Button>
       </div>
     </div>
   </Layout>
