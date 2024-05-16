86810f9e143186a4283fdc836cf3ab8d89ff1677
BriqueAuBob <briqueaubob@gmail.com>
2024-05-16 16:47:02 +0200

    :bug: Fix dark mode not applying until we go on account page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,17 @@
+<script setup lang="ts">
+import { useColorMode } from '@vueuse/core';
+
+useColorMode({
+  attribute: 'class',
+  modes: {
+    light: '',
+    dark: 'dark',
+  },
+  storageKey: 'theme',
+  disableTransition: false,
+});
+</script>
+
 <template>
   <RouterView />
 </template>
